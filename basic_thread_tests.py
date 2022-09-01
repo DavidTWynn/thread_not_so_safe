48b2201ed5981cee8005366424356007caed8817
david <davidtwynn@gmail.com>
2022-08-31 22:38:40 -0400

    Tried to see if conncurent futures submit behaved differently in 3.10. I see 3.10 works well with adding integers (this appears to be because of a CPython change). The concurrent.futures actually triggers at a lower iteration number. Could this have something to do with concurrent futures  being able to start building another thread while working on bringing up another?

--- basic_thread_tests.py
+++ basic_thread_tests.py
@@ -1,3 +1,4 @@
+from concurrent.futures import ThreadPoolExecutor
 from threading import Thread
 import sys
 
@@ -8,7 +9,7 @@ class Adder:
     def __init__(self):
         self.counter = 0
 
-    def increment(self):
+    def increment(self, *args):
         for _ in range(self.iterations):
             self.counter += 1
 
@@ -18,15 +19,41 @@ def run_threaded_add(num_threads):
 
     threads = []
     for _ in range(num_threads):
-        threads.append(Thread(target=adder.increment))
-
-    for thread in threads:
+        thread = Thread(target=adder.increment)
         thread.start()
+        threads.append(thread)
 
     for thread in threads:
         thread.join()
 
     return adder.counter
+
+
+def run_concurrent_thread_add(num_threads):
+    adder = Adder()
+
+    with ThreadPoolExecutor(max_workers=num_threads) as executor:
+        results = []
+        for _ in range(num_threads):
+            results.append(executor.submit(adder.increment))
+
+    return adder.counter
+
+
+def check_threaded_adder(runs, num_threads):
+    for _ in range(runs):
+        result = run_threaded_add(num_threads)
+        expected = num_threads * Adder.iterations
+        if result != expected:
+            print(f"Issue found: {result} but expected {expected}")
+
+
+def check_concurrent_thread_adder(runs, num_threads):
+    for _ in range(runs):
+        result = run_concurrent_thread_add(num_threads)
+        expected = num_threads * Adder.iterations
+        if result != expected:
+            print(f"Concurrent issue found: {result} but expected {expected}")
 
 
 def main():
@@ -37,11 +64,10 @@ def main():
     # Run test multiple times and verify function end result
     runs = 3
     num_threads = 10
-    for _ in range(runs):
-        result = run_threaded_add(num_threads)
-        expected = num_threads * Adder.iterations
-        if result != expected:
-            print(f"Issue found: {result} but expected {expected}")
+    check_threaded_adder(runs, num_threads)
+
+    # Run test multiple times and verify function end result
+    check_concurrent_thread_adder(runs, num_threads)
 
 
 if __name__ == "__main__":
